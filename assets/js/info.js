8e1691ae05f3cdf562802e610c5d0c4b1295cdab
jeanmends <jeanmenreis@hotmail.com>
2023-10-09 21:00:28 -0300

    css e pop-up ajustados

--- assets/js/info.js
+++ assets/js/info.js
@@ -1,4 +1,18 @@
 console.log("funcionou");
+function toggleShinyNormal(){
+    let toggle = document.getElementById("toggle-shiny-normal");
+    let normal = document.getElementById("normal");
+    let shiny = document.getElementById("shiny");
+        if (toggle.innerHTML == "shiny"){
+            normal.style.display = 'none';
+            shiny.style.display = 'block';
+            toggle.innerHTML = 'normal'
+        }else{
+            normal.style.display = 'block';
+            shiny.style.display = 'none';
+            toggle.innerHTML = 'shiny'
+        }
+}
 
 document.body.addEventListener('click', function (evt) {
     if (evt.target.classList.contains('more')) {
@@ -12,12 +26,37 @@ document.body.addEventListener('click', function (evt) {
         .then((jsonBody) => {
             const types = jsonBody.types.map((typeSlot) => typeSlot.type.name)
             const [type] = types
-            informationInside.innerHTML = `
+            informationInside.innerHTML += `
+            
             <div class='finally-info ${type}'>
-                <h1>${jsonBody.name}</h1>
+            
+                <div class='head-info'>
+                    <img src="assets/img/left-arrow.png" class="left-arrow" />
+                    <h1>${jsonBody.name}</h1>
+                    <h6>#${('0000'+jsonBody.id).slice(-4)}</h6>
+                </div>
+                <div class="big-img">
+                <button id="toggle-shiny-normal" onclick="toggleShinyNormal()">shiny</button>
+                    <div class="pokemon-version">
+                        <img src='${jsonBody.sprites.other.home.front_default}' id="normal" />
+                        <img src='${jsonBody.sprites.other.home.front_shiny}' id="shiny" />
+                    </div>  
+                </div>
+            </div>    
+                
             
             `
+          // let toggle = document.getElementById("toggle-shiny-normal");
+           // toggle.addEventListener("click", toggleShinyNormal);
         })
         
     }
+
 }, false)
+
+
+
+
+
+
+
